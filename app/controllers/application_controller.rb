a11f9c1b12ba8d93bf231d9384200f8ccdd70a9d
Chihiro <Chi.r.r.333@gmail.com>
2020-07-02 22:53:21 +0900

    Settings of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # ログイン後
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
+  # ログアウト後
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
