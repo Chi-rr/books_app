631e313f4acd777c6276b84e642f6dc1af60f10b
Chihiro <Chi.r.r.333@gmail.com>
2020-07-03 15:29:19 +0900

    Delete default value

--- db/migrate/20200701081201_devise_create_users.rb
+++ db/migrate/20200701081201_devise_create_users.rb
@@ -4,12 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
+      t.string :name,               null: false
       t.integer :postal_code
       t.string :address
       t.text :profile
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,12 @@ ActiveRecord::Schema.define(version: 2020_07_01_081201) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.integer "postal_code"
     t.string "address"
     t.text "profile"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
