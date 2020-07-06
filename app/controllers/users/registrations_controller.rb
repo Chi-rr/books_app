df61389212a76fb31048015cc950521d8060ef96
Chihiro <Chi.r.r.333@gmail.com>
2020-07-06 21:53:00 +0900

    Implement image upload

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -57,7 +57,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :postal_code, :address, :profile])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :postal_code, :address, :profile, :avatar])
     end
 
     # プロフィール編集時、password入力をなくす
