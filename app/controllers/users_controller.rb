e64f50d01ddb8a8bac33c173cf8043bca1c244bd
Chihiro <Chi.r.r.333@gmail.com>
2020-07-14 17:01:50 +0900

    Add follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+  end
+
+  def following
+    @title = "フォロー"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render "show_follow"
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render "show_follow"
   end
 end
