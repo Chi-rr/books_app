0d924651091650804b0379022e439a33283b8eb2
Chihiro <Chi.r.r.333@gmail.com>
2020-07-14 16:42:56 +0900

    Associate User with Relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id",
+            dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
@@ -24,5 +34,18 @@ class User < ApplicationRecord
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
