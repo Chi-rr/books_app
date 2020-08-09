0c2b3e564718743cc2d73a00aa9afc0764716eae
Chihiro <Chi.r.r.333@gmail.com>
2020-08-09 14:28:30 +0900

    Fix assertions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,9 +4,9 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user1 = users(:user_a)
-    @user2 = users(:user_b)
-    @user3 = users(:user_c)
+    @user1 = users(:user_1)
+    @user2 = users(:user_2)
+    @user3 = users(:user_3)
   end
 
   test "githubでログイン時、登録済みのユーザー情報を取得する" do
@@ -15,7 +15,7 @@ class UserTest < ActiveSupport::TestCase
       provider: @user1.provider,
     })
     auth_user = User.find_for_github_oauth(auth)
-    assert @user1 == auth_user
+    assert_equal(auth_user, @user1, "#{auth_user.name}を期待しましたが、#{@user2.name}でした")
   end
 
   test "githubでログイン時、登録済みのユーザー情報と照合が取れなかった場合に新規登録する" do
@@ -25,7 +25,7 @@ class UserTest < ActiveSupport::TestCase
     })
     auth_user = User.find_for_github_oauth(auth)
     new_user = User.find_by(email: "123-github@example.com")
-    assert auth_user == new_user
+    assert_equal(new_user, auth_user)
   end
 
   test "ダミーのemailを作成できる" do
@@ -34,14 +34,14 @@ class UserTest < ActiveSupport::TestCase
       provider: @user1.provider
     })
     email = User.dummy_email(auth)
-    assert email == "12348765-github@example.com"
+    assert_equal("12348765-github@example.com", email)
   end
 
   test "他のユーザーをフォローできる" do
     assert_difference "@user2.following.count" do
       @user2.follow(@user3)
     end
-    assert @user3.followers.count == 2
+    assert_equal(2, @user3.followers.count)
     assert @user2.following?(@user3)
   end
 
@@ -50,5 +50,6 @@ class UserTest < ActiveSupport::TestCase
     assert_difference "@user1.following.count", -1 do
       @user1.unfollow(@user3)
     end
+    assert_equal(0, @user1.following.count)
   end
 end
